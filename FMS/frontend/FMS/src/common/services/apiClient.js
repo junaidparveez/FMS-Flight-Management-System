3ae536634f8f0a5924af6128a3b0d91ed0ad505e
junaidparveez <junaidparwez@gmail.com>
2025-07-24 23:54:34 +0530

    Update apiClient.js

--- FMS/frontend/FMS/src/common/services/apiClient.js
+++ FMS/frontend/FMS/src/common/services/apiClient.js
@@ -1,12 +1,10 @@
 import axios from 'axios';
 import { getToken, refreshToken, clearAuth } from '../utils/auth';
 const apiClient = axios.create({
-  // either of these two is fine:
   baseURL: process.env.REACT_APP_API_URL,
-  // —or—
-  // baseURL: `${process.env.REACT_APP_API_URL}`,
   timeout: 10000,
 });
+
 
 apiClient.interceptors.request.use((config) => {
   const token = getToken();
