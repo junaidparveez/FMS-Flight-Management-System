2af7d42e1056ae0ddf1bc87779505792f5bd08dc
junaidparveez <junaidparwez@gmail.com>
2025-07-25 10:45:34 +0530

    Update App.jsx

--- FMS/frontend/FMS/src/App.jsx
+++ FMS/frontend/FMS/src/App.jsx
@@ -1,12 +1,32 @@
-import React from 'react';
-import AppRouter from './AppRouter';
-import { AuthProvider } from './common/context/AuthContext';
-import { ThemeProvider } from './common/context/ThemeContext';
-import NotificationProvider from './common/components/NotificationProvider';
-
+import React, { useEffect } from "react";
+import AppRouter from "./AppRouter";
+import { AuthProvider } from "./common/context/AuthContext";
+import { ThemeProvider } from "./common/context/ThemeContext";
+import NotificationProvider from "./common/components/NotificationProvider";
+import { useNavigate } from "react-router-dom";
 function App() {
+  let navigate = useNavigate();
+  useEffect(() => {
+    function clearLocalStorageAndLogout() {
+      alert(
+        "Your session has expired. You have been logged out for security reasons."
+      );
+      navigate("/login");
+      localStorage.clear();
+    }
+
+    function scheduleClear() {
+      return window.setTimeout(() => {
+        clearLocalStorageAndLogout();
+        scheduleClear();
+      }, 10 * 60 * 1000); 
+    }
+
+    const timeoutId = scheduleClear();
+
+    return () => clearTimeout(timeoutId);
+  }, []);
   return (
-   
     <NotificationProvider>
       <AuthProvider>
         <ThemeProvider>
